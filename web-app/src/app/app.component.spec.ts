800e69f76128f87d636f2691172e8a75b2758cec
John Christiansen <johnchristiansen@gmail.com>
2025-10-17 13:57:40 -0600

    feat(devops) ci/cd

--- web-app/src/app/app.component.spec.ts
+++ web-app/src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('BYU 590R Monorepo');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'BYU 590R Monorepo'
+    );
   });
 });
